d338df4badefa6a5af40316651136b27c47cb1b6
ksuwannaarj <87976073+ksuwannaarj@users.noreply.github.com>
2022-04-23 02:43:26 +1000

    changed api

--- greenpath/src/GetRoutes.js
+++ greenpath/src/GetRoutes.js
@@ -64,7 +64,7 @@ function Route(props) {
 
 class GetRoutes extends React.Component {
   async fetchFromApi(addressOrigin, addressDestination) {
-    const fetchAddress = `http://localhost:8080/direction?destination=${addressDestination}&origin=${addressOrigin}`;
+    const fetchAddress = `https://greenpath-3ivy5.ondigitalocean.app/direction?destination=${addressDestination}&origin=${addressOrigin}`;
     this.props.onLoading(true);
 
     const response = await fetch(fetchAddress);
